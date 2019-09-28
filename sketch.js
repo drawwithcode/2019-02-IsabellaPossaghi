e8df578f3619a99657eff56566e74f46108f826b
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2019-09-28 11:33:59 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,50 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight)
+  angleMode(DEGREES);
+  noStroke();
+  frameRate(10)
+
+
 }
 
 function draw() {
-  // put drawing code here
+  background("black");
+
+  for (a = 0; a <= 30; a++) {
+    for (b = 0; b <= 30; b++) {
+      let x = a * width / 30,
+        y = b * height / 30,
+
+        angle = 20 * a + 20 * b + frameCount * 50,
+        r = 20,
+
+
+        lerp = Math.abs(cos(angle)),
+        colors = lerpColor(color("gold"), color("slateblue"), lerp);
+      colors1 = lerpColor(color("greenyellow"), color("deeppink"), lerp);
+
+      let d = map(mouseX, 0, width, 0, 20);
+
+
+      ellipse(x, y, d);
+
+
+
+
+
+      if (mouseIsPressed) {
+        fill(colors);
+
+      } else {
+        fill(colors1);
+      }
+
+      text('Move the cursor and click!', 50, 50);
+      textSize(20);
+
+
+
+    }
+  }
+
 }
